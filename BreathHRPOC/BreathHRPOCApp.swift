34d176ef882140dfe6c5932a7819489e793e95f6
pjoyce1995 <82143724+pjoyce1995@users.noreply.github.com>
2026-06-15 15:38:11 -0400

    Initial Commit

---
+++ BreathHRPOC/BreathHRPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathHRPOCApp.swift
+//  BreathHRPOC
+//
+//  Created by Peter Joyce on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BreathHRPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreathHRPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BreathHRPOC
+//
+//  Created by Peter Joyce on 15/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
